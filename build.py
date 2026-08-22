90d8e4b26895f9b2e53be133092fdd10057cda97
夏村洋平 <d51498go@icloud.com>
2026-08-22 17:22:26 +0900

    BlueChat: BlueVoom化・LINE表記除去・称号ゴールド・名前変更ボタン・別端末同期UI改善

--- build.py
+++ build.py
@@ -17,7 +17,7 @@ PAGES_HEAD = """<!DOCTYPE html>
   <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
   <meta http-equiv="Pragma" content="no-cache">
   <meta http-equiv="Expires" content="0">
-  <title>BlueChatX</title>
+  <title>BlueChat</title>
   <script>
 (function () {
   var host = String(location.hostname || '').toLowerCase();
